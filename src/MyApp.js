05c58008aeb15c6183c54e894be157b3f72b614b
taiminhle4920 <taiminhle4920@gmail.com>
2022-09-23 13:20:50 -0700

    finish the button

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,25 +1,8 @@
 import React, {useState} from 'react';
 import Table from './Table';
-
+import Form from './Form';
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-      name: 'Charlie',
-      job: 'Janitor',
-    },
-    {
-      name: 'Mac',
-      job: 'Bouncer',
-    },
-    {
-      name: 'Dee',
-      job: 'Aspring actress',
-    },
-    {
-      name: 'Dennis',
-      job: 'Bartender',
-    },
-    ]);  
+  const [characters, setCharacters] = useState([]);  
 
 function removeOneCharacter (index) {
   const updated = characters.filter((character, i) => {
@@ -30,7 +13,14 @@ function removeOneCharacter (index) {
   return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />
     </div>
   )
+
+  function updateList(person) {
+    setCharacters([...characters, person]);
+    
+    
+  }
 }
 export default MyApp;
